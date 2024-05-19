93fa35006f8d75d556164fba03d3c8e8b8f4f8a5
Kishan-Jaiswar <jaiswarkishan78@gmail.com>
2024-05-19 10:17:45 +0530

    deleted package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import Hero from './components/Hero';
 import OurServices from './components/OurServices';
 import WeBelieve from './components/WeBelieve';
 import RecentWorks from './components/RecentWorks';
-import Testimonial from './components/Testimonial';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
